247252f06253f2e81e72191e220283c2b89a5e51
Connor Penrod <cfpvf9@mail.missouri.edu>
2019-04-30 22:01:02 -0500

    Worked on concatenation script with arcsec radius search

--- april/script.py
+++ april/script.py
@@ -1,7 +1,25 @@
 import csv
+import math
 
 unique_rows = {}
 id = 1
+ARCSEC = 0.000277778
+
+def search_radius(x1, y1, x2, y2, r = ARCSEC):
+    dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    if dist <= r:
+        return True
+    else:
+        return False
+
+def checkUnique(tup):
+    for key in unique_rows.keys():
+        if search_radius(float(tup[0]), float(tup[1]),float(unique_rows[key][1]),float(unique_rows[key][2])):
+            return key
+    return None
+        
+radecpairs = [] 
+
 with open('catalogs1.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     ra = 7
@@ -9,13 +27,13 @@ with open('catalogs1.csv', newline='') as csvfile:
     z = 6
     ew = 5
     cat = 1
-    
+   
     for row in spamreader:
-        if((row[ra], row[dec]) not in unique_rows):
+        if checkUnique((row[ra], row[dec])) == None:
             unique_rows[(row[ra], row[dec])] = [id, row[ra], row[dec], row[z], row[ew], cat, 0]
             id += 1
         else:
-            found_row = unique_rows[(row[ra], row[dec])]
+            found_row = unique_rows[checkUnique((row[ra], row[dec]))]
             found_row[6] += 1
             found_row[5] = str(found_row[5]) + "//" + str(cat)
             if float(row[z]) > float(found_row[3]):
@@ -23,7 +41,7 @@ with open('catalogs1.csv', newline='') as csvfile:
             if float(row[ew]) > float(found_row[4]):
                 found_row[4] = row[ew]
                 
-            unique_rows[(row[ra], row[dec])] = found_row
+            unique_rows[checkUnique((row[ra], row[dec]))] = found_row
             
 with open('catalogs2.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -34,11 +52,11 @@ with open('catalogs2.csv', newline='') as csvfile:
     cat = 2
     
     for row in spamreader:
-        if((row[ra], row[dec]) not in unique_rows):
+        if checkUnique((row[ra], row[dec])) == None:
             unique_rows[(row[ra], row[dec])] = [id, row[ra], row[dec], -1, row[ew], cat,0]
             id += 1
         else:
-            found_row = unique_rows[(row[ra], row[dec])]
+            found_row = unique_rows[checkUnique((row[ra], row[dec]))]
             found_row[6] += 1
             found_row[5] = str(found_row[5]) + "//" + str(cat)
             if float(row[z]) > float(found_row[3]):
@@ -46,7 +64,7 @@ with open('catalogs2.csv', newline='') as csvfile:
             if float(row[ew]) > float(found_row[4]):
                 found_row[4] = row[ew]
                 
-            unique_rows[(row[ra], row[dec])] = found_row
+            unique_rows[checkUnique((row[ra], row[dec]))] = found_row
             
 with open('catalogs3.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -56,11 +74,11 @@ with open('catalogs3.csv', newline='') as csvfile:
     ew = 4
     cat = 3
     for row in spamreader:
-        if((row[ra], row[dec]) not in unique_rows):
+        if checkUnique((row[ra], row[dec])) == None:
             unique_rows[(row[ra], row[dec])] = [id, row[ra], row[dec], row[z], row[ew], cat,0]
             id += 1
         else:
-            found_row = unique_rows[(row[ra], row[dec])]
+            found_row = unique_rows[checkUnique((row[ra], row[dec]))]
             found_row[6] += 1
             found_row[5] = str(found_row[5]) + "//" + str(cat)
             if float(row[z]) > float(found_row[3]):
@@ -68,8 +86,8 @@ with open('catalogs3.csv', newline='') as csvfile:
             if float(row[ew]) > float(found_row[4]):
                 found_row[4] = row[ew]
                 
-            unique_rows[(row[ra], row[dec])] = found_row
-
+            unique_rows[checkUnique((row[ra], row[dec]))] = found_row
+            
 with open('catalogs4.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     ra = 8
@@ -79,11 +97,11 @@ with open('catalogs4.csv', newline='') as csvfile:
     cat = 4
     
     for row in spamreader:
-        if((row[ra], row[dec]) not in unique_rows):
+        if checkUnique((row[ra], row[dec])) == None:
             unique_rows[(row[ra], row[dec])] = [id, row[ra], row[dec], row[z], row[ew], cat,0]
             id += 1
         else:
-            found_row = unique_rows[(row[ra], row[dec])]
+            found_row = unique_rows[checkUnique((row[ra], row[dec]))]
             found_row[6] += 1
             found_row[5] = str(found_row[5]) + "//" + str(cat)
             if float(row[z]) > float(found_row[3]):
@@ -91,7 +109,7 @@ with open('catalogs4.csv', newline='') as csvfile:
             if float(row[ew]) > float(found_row[4]):
                 found_row[4] = row[ew]
                 
-            unique_rows[(row[ra], row[dec])] = found_row
+            unique_rows[checkUnique((row[ra], row[dec]))] = found_row
 with open('catalogs5.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     ra = 43
@@ -101,11 +119,11 @@ with open('catalogs5.csv', newline='') as csvfile:
     cat = 5
     
     for row in spamreader:
-        if((row[ra], row[dec]) not in unique_rows):
+        if checkUnique((row[ra], row[dec])) == None:
             unique_rows[(row[ra], row[dec])] = [id, row[ra], row[dec], row[z], -1, cat,0]
             id += 1
         else:
-            found_row = unique_rows[(row[ra], row[dec])]
+            found_row = unique_rows[checkUnique((row[ra], row[dec]))]
             found_row[6] += 1
             found_row[5] = str(found_row[5]) + "//" + str(cat)
             if float(row[z]) > float(found_row[3]):
@@ -113,7 +131,7 @@ with open('catalogs5.csv', newline='') as csvfile:
             if float(row[ew]) > float(found_row[4]):
                 found_row[4] = row[ew]
                 
-            unique_rows[(row[ra], row[dec])] = found_row
+            unique_rows[checkUnique((row[ra], row[dec]))] = found_row
 with open('catalogs6.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     ra = 7
@@ -123,11 +141,11 @@ with open('catalogs6.csv', newline='') as csvfile:
     cat = 6
     
     for row in spamreader:
-        if((row[ra], row[dec]) not in unique_rows):
+        if checkUnique((row[ra], row[dec])) == None:
             unique_rows[(row[ra], row[dec])] = [id, row[ra], row[dec], row[z], row[ew], cat,0]
             id += 1
         else:
-            found_row = unique_rows[(row[ra], row[dec])]
+            found_row = unique_rows[checkUnique((row[ra], row[dec]))]
             found_row[6] += 1
             found_row[5] = str(found_row[5]) + "//" + str(cat)
             if float(row[z]) > float(found_row[3]):
@@ -135,10 +153,10 @@ with open('catalogs6.csv', newline='') as csvfile:
             if float(row[ew]) > float(found_row[4]):
                 found_row[4] = row[ew]
                 
-            unique_rows[(row[ra], row[dec])] = found_row
+            unique_rows[checkUnique((row[ra], row[dec]))] = found_row
 
 with open('res_catalog.csv', 'w+') as f:
-    f.write("ID, RA, DEC, Z, EW, CatID\n")
+    f.write("ID, RA, DEC, Z, EW, CatID, NumDuplicates\n")
     for key in unique_rows:
         str_inst = [str(x) for x in unique_rows[key]]
         f.write(",".join(str_inst) + "\n")
